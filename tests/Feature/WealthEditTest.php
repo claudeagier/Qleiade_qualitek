94f48af9dea65aeca88e2bc8e1acc6cda593b209
claudeagier <claude.agier@gmail.com>
2022-06-07 15:32:21 +0200

    setup test and test wealth

---
+++ tests/Feature/WealthEditTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Orchid\Support\Testing\ScreenTesting;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use App\Models\Wealth;
+use Tests\TestCase;
+use App\Models\User;
+use Tests\InteractsWithUsers;
+
+use function PHPUnit\Framework\assertEquals;
+
+class WealthEditTest extends TestCase
+{
+    use ScreenTesting, InteractsWithUsers;
+    // use WithoutMiddleware;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_display_wealth_screen()
+    {
+        $this->withoutMiddleware();
+        $screen = $this->screen('platform.quality.wealths.create');
+
+        $screen->display()
+            ->assertStatus(302);
+    }
+
+    public function test_create_wealth(){
+        // $this->app['env'] = 'production';
+        $this->setUsertest();
+
+        $parameters =  [
+            'name' => 'test auto 1',
+            'conformity_level' => 'high',
+            'wealth_type' => '2',
+            'description' => 'test automatique de création',
+            'indicators' => ['1'],
+            'processus' => '1',
+            'actions' => ['3'],
+            'careers' => ['2'],
+            'formations' => ['3'],
+            'validity_date' => '22-06-2022',
+        ];
+        $screen = $this->screen('platform.quality.wealths.create');
+        $screen
+            ->actingAs($this->user)
+            ->method('save', $parameters)
+            ->assertStatus(200);
+    }
+
+    public function test_update_wealth(){
+        // $this->app['env'] = 'production';
+        $this->setUsertest();
+        $id = Wealth::latest()->first()->id;
+        $params = [            
+            'name' => 'test auto 1',
+            'conformity_level' => 'high',
+            'wealth_type' => '2',
+            'description' => 'test automatique de création',
+            'indicators' => ['1'],
+            'processus' => '1',
+            'actions' => ['3'],
+            'careers' => ['2'],
+            'formations' => ['3'],
+            'validity_date' => '22-06-2022',
+        ];
+        $screen = $this->screen('platform.quality.wealths.create');
+        $res = $screen
+            ->actingAs($this->user)
+            ->method('save', $params);
+        
+        $res->assertStatus(200);
+    }
+
+    public function test_delete_wealth(){
+        $this->setUsertest();
+        $id = Wealth::latest()->first()->id;
+        $params = [
+            'id' => $id
+        ];
+        $screen = $this->screen('platform.quality.wealths');
+        $screen
+            ->actingAs($this->user)
+            ->method('remove', $params)
+            ->assertStatus(200);
+        
+    }
+}
