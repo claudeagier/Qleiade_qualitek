da9aa43496402782d1d724438cb3517cc29e911f
claudeagier <claude.agier@gmail.com>
2022-07-04 11:54:16 +0200

    clean code Traits

--- app/Http/Traits/CSVSeeder.php
+++ app/Http/Traits/CSVSeeder.php
@@ -3,13 +3,13 @@
 namespace App\Http\Traits;
 
 trait CSVSeeder {
-
 	public function csv_to_array($filename = '', $delimiter = ',') {
-		if (!file_exists($filename) || !is_readable($filename))
+		if (!file_exists($filename) || !is_readable($filename)){
 			return FALSE;
+		}
 
 		$header = NULL;
-		$data = array();
+		$data = [];
 		if (($handle = fopen($filename, 'r')) !== FALSE) {
 			while (($row = fgetcsv($handle, 1000, $delimiter)) !== FALSE) {
 				if (!$header) {
@@ -17,16 +17,12 @@ trait CSVSeeder {
 					$header[] = "created_at";
 				}
 				else {
-					// $toto = array_combine($header, $row);
-					// $toto["created_at"] = now()->format('Y-m-d H:i:s');
 					$row[] = now();
 					$data[] = array_combine($header, $row);
 				}
 			}
 			fclose($handle);
 		}
-		// dd($data);
 		return $data;
 	}
-
 }
